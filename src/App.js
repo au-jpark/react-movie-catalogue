9691641238d2868affe7f7793387532e1df6de8b
J Park <bjh418@hotmail.com>
2018-06-24 23:14:29 -0400

    Add movie slots. Add dummy movie list for development

--- src/App.js
+++ src/App.js
@@ -1,14 +1,65 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from "./components/SearchBar";
+import MovieSlot from "./components/MovieSlot";
 
 class App extends Component {
+
+  state = {};
+
+  componentWillMount() {
+    const movies = [
+      {
+        title_english: "Snowpiercer",
+        large_cover_image: "https:\\/\\/yts.am\\/assets\\/images\\/movies\\/Snowpiercer_2013\\/large-cover.jpg",
+        id: "",
+        genres: ["Action","Adventure","Sci-Fi","Thriller"],
+        synopsis: ""
+      },
+      {
+        title_english: "Avengers",
+        large_cover_image: "https:\\/\\/yts.am\\/assets\\/images\\/movies\\/avengers_age_of_ultron_2015\\/large-cover.jpg",
+        id: "",
+        genres: ["Action","Adventure","Sci-Fi","Thriller"],
+        synopsis: ""
+      },
+      {
+        title_english: "Captain America",
+        large_cover_image: "https:\\/\\/yts.am\\/assets\\/images\\/movies\\/Captain_America_The_Winter_Soldier_2014\\/large-cover.jpg",
+        id: "",
+        genres: ["Action","Adventure","Sci-Fi","Thriller"],
+        synopsis: ""
+      }
+    ];
+    this.setState({
+      movies
+    });
+  }
+
+  renderMovieSlot = () => {
+    const movies = this.state.movies.map(movie => {
+      return (
+        <MovieSlot
+          title={movie.title_english}
+          poster={movie.large_cover_image}
+          key={movie.id}
+          genres={movie.genres}
+          synopsis={movie.synopsis}
+        />
+      );
+    });
+    return movies;
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Movie Catalogue</h1>
         </header>
+        <div className="App-content">
+          {this.renderMovieSlot()}
+        </div>
         <footer className="App-footer">
           <SearchBar className="SearchBar">
           </SearchBar>
---
+++ src/components/MovieSlot.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis';
+import '../MovieSlot.css';
+
+class MovieSlot extends Component {
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+  }
+
+  static defaultProps = {
+    title: "",
+    poster: "",
+    genres: [],
+    synopsis: ""
+  }
+
+  render() {
+    return (
+      <div className="Movie">
+        <div className="Movie__Column">
+          <img src={this.props.poster} title={this.props.title} className="Movie__Poster" />
+        </div>
+        <div className="Movie__Column">
+          <h1>{this.props.title}</h1>
+          <div className="Movie__Genres">
+            {this.props.genres.map((genre) => <span className="Movie__Genre">{genre}</span>)}
+          </div>
+          <div className="Movie__Synopsis">
+            <LinesEllipsis
+              text={this.props.synopsis}
+              maxLine='3'
+              ellipsis='...'
+              trimRight
+              basedOn='letters'
+            />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MovieSlot
